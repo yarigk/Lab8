bd33ebae3649226321626478b2b9f1fba864fce7
Мурзиков Ярослав <woolfood@hotmail.com>
2015-05-21 21:37:00 +0300

    N

---
+++ ItWillBeBetter/Controllers/SecondController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ItWillBeBetter.Controllers
+{
+    public class SecondController : Controller
+    {
+        // GET: Second
+        public ActionResult Index()
+        {
+            ViewBag.message = DateTime.Now;
+            return View();
+        }
+    }
+}
---
+++ ItWillBeBetter/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ItWillBeBetter.Startup))]
+namespace ItWillBeBetter
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
